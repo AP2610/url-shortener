2f9ec9357f7e6f2a7ea951b3b99124f283edd802
Arjun Puri <arjun_261094@hotmail.com>
2025-06-24 16:24:04 +0200

    Setup - prettier - eslint - base layout - husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { MyLink } from '@/components/ui/my-link';
 
 const Home = () => {
   return (
-    <div className="container mx-auto flex h-full flex-col items-center justify-center px-4 pt-[var(--header-height)] pb-10">
+    <div className="container mx-auto flex h-full flex-col items-center justify-center px-4 pb-10 pt-[var(--header-height)]">
       <EntryAnimation />
       <AnimatedElementPresence shouldWaitToAnimateFor="entry-animation">
         <div className="mb-8 flex flex-col items-center justify-center space-y-8 text-center md:mb-16">
@@ -20,7 +20,7 @@ const Home = () => {
 
       {/* TODO: Guard with auth */}
       <AnimatedElementPresence shouldWaitToAnimateFor="entry-animation">
-        <p className="mt-8 text-center text-dark-gray">
+        <p className="text-dark-gray mt-8 text-center">
           Want to keep track of your shortened links? <MyLink href="/sign-up">Sign up!</MyLink>.
         </p>
       </AnimatedElementPresence>
