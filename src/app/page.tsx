4db801a52d609becff9a6a2912b5cfb643e7d572
Arjun Puri <arjun_261094@hotmail.com>
2025-06-28 00:15:07 +0200

    Fix broken build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
 import { UrlForm } from '@/components/features/url-form';
-import { EntryAnimation } from '@/components/ui/entry-animation';
+import { AnimatedElementPresence } from '@/components/ui/animation/animated-element-presence';
+import { EntryAnimation } from '@/components/ui/animation/entry-animation';
+import { Heading } from '@/components/ui/heading';
+import { MyLink } from '@/components/ui/my-link';
 
 const Home = () => {
   return (
     <div className="container mx-auto flex h-full flex-col items-center justify-center pt-[var(--header-height)]">
       <EntryAnimation />
+
+      <AnimatedElementPresence shouldWaitToAnimateFor="entry-animation">
+        <div className="mb-16 flex flex-col items-center justify-center space-y-8">
+          <Heading level="h1">Got a long URL? Shortly it!</Heading>
+          <p className="text-center text-dark-gray">
+            Shorten your links with our URL shortening service. It's free and easy to use.
+          </p>
+        </div>
+      </AnimatedElementPresence>
+
       <UrlForm />
+
+      {/* TODO: Guard with auth */}
+      <AnimatedElementPresence shouldWaitToAnimateFor="entry-animation">
+        <p className="mt-8 text-center text-dark-gray">
+          Want to keep track of your shortened links? <MyLink href="/sign-up">Sign up!</MyLink>.
+        </p>
+      </AnimatedElementPresence>
     </div>
   );
 };
