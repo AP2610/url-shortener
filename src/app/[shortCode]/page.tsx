362e5162d316df7ff2449fea2cb7f78192b0385b
Arjun Puri <arjun_261094@hotmail.com>
2025-06-29 17:30:11 +0200

    set last visited date

--- src/app/[shortCode]/page.tsx
+++ src/app/[shortCode]/page.tsx
@@ -1,5 +1,6 @@
 import { getUrlRecord } from '@/actions/db/get-url-record';
 import { incrementVisitCount } from '@/actions/db/increment-visit-count';
+import { setLastVisited } from '@/actions/db/set-last-visited';
 import { redirect } from 'next/navigation';
 
 interface ShortUrlPageProps {
@@ -20,6 +21,9 @@ export default async function ShortUrlPage({ params }: ShortUrlPageProps) {
 
   if (url && !hasError) {
     await incrementVisitCount(shortCode);
+
+    // TODO: Backfill last visited at for all url records
+    await setLastVisited({ shortCode });
     redirect(url.url);
   } else {
     // TODO: Show modal to user with error message and button to go back to home page
