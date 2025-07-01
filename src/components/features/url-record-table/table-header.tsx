8651da8364316106c2372b3b3250b60a8afb802a
Arjun Puri <arjun_261094@hotmail.com>
2025-07-02 00:57:31 +0200

    add className to header

--- src/components/features/url-record-table/table-header.tsx
+++ src/components/features/url-record-table/table-header.tsx
@@ -9,12 +9,16 @@ interface TableHeaderProps {
 }
 
 export const TableHeader = ({ children, className, width }: TableHeaderProps) => {
-  const tableHeaderClasses = cn('resize-x overflow-hidden border border-dark-gray px-4 py-2', {
-    'w-16': width === 'small',
-    'w-24': width === 'medium',
-    'w-32': width === 'large',
-    'w-64': width === 'xl',
-  });
+  const tableHeaderClasses = cn(
+    'resize-x overflow-hidden border border-dark-gray px-4 py-2',
+    {
+      'w-16': width === 'small',
+      'w-24': width === 'medium',
+      'w-32': width === 'large',
+      'w-64': width === 'xl',
+    },
+    className,
+  );
 
   return <th className={cn(tableHeaderClasses, className)}>{children}</th>;
 };
