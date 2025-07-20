70e5b5912a92474533f29bc8d5c7e989a58700f9
Arjun Puri <arjun_261094@hotmail.com>
2025-07-21 01:05:34 +0200

    Add code input component and style verification form

--- src/components/features/authentication/verification-form/index.tsx
+++ src/components/features/authentication/verification-form/index.tsx
@@ -1,25 +1,37 @@
 'use client';
 
-import { useState } from 'react';
+import { DotLoader } from '@/components/ui/animation/dot-loader';
+import { Button } from '@/components/ui/button';
+import { CodeInput } from '@/components/ui/forms/code-input';
+import { Heading } from '@/components/ui/heading';
 
 interface VerificationFormProps {
   handleVerify: (e: React.FormEvent<HTMLFormElement>, code: string) => void;
+  isLoading: boolean;
 }
 
-// TODO: Add loading state when submitting the form
-// TODO: Style the form
-// TODO: Handle paste in OTP field. Create 6 inputs, pasting into each one. Create hidden input for whole code.
-export const VerificationForm = ({ handleVerify }: VerificationFormProps) => {
-  const [code, setCode] = useState('');
+export const VerificationForm = ({ handleVerify, isLoading }: VerificationFormProps) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target as HTMLFormElement);
+    const code = formData.get('code') as string;
+    handleVerify(event, code);
+  };
 
   return (
-    <>
-      <h1>Verify your email</h1>
-      <form onSubmit={(e) => handleVerify(e, code)}>
-        <label id="code">Enter your verification code</label>
-        <input value={code} id="code" name="code" onChange={(e) => setCode(e.target.value)} />
-        <button type="submit">Verify</button>
-      </form>
-    </>
+    <form className="w-full space-y-8 p-8 md:w-90" onSubmit={handleSubmit}>
+      <Heading level="h2">Verify your email</Heading>
+
+      <label className="mb-4 block" htmlFor="code">
+        Enter your verification code
+      </label>
+
+      <CodeInput length={6} hiddenInputName="code" />
+
+      <Button variant="primary" className="w-full" disabled={isLoading}>
+        {isLoading ? <DotLoader color="white" /> : 'Verify'}
+      </Button>
+    </form>
   );
 };
