a76e6e94f0bc314a3104c2a6e369f47a510f190a
Arjun Puri <arjun_261094@hotmail.com>
2025-08-01 01:34:58 +0200

    Update shortcode matcher to accoount for _ and - in generated code

--- src/lib/utils/shortcode-utils.ts
+++ src/lib/utils/shortcode-utils.ts
@@ -1,4 +1,4 @@
-export const SHORTCODE_REGEX = /^_([a-zA-Z0-9]{6})$/;
+export const SHORTCODE_REGEX = /^_([a-zA-Z0-9_-]{6})$/;
 
 export const isValidShortcode = (shortCode: string): boolean => {
   return SHORTCODE_REGEX.test(shortCode);
--- src/middleware.ts
+++ src/middleware.ts
@@ -42,7 +42,7 @@ export default clerkMiddleware(async (auth, request: NextRequest) => {
 export const config = {
   matcher: [
     // Short code matcher
-    '/(_[a-zA-Z0-9]+)',
+    '/(_[a-zA-Z0-9_-]+)',
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
